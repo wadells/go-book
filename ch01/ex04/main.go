16e063033d3ae8486d6c91251eb525df3456a7d9
Walt Della <walt@javins.net>
2020-07-06 20:07:06 -0700

    Add solution to exercise 1.4.

--- ch01/ex04/main.go
+++ ch01/ex04/main.go
@@ -12,7 +12,7 @@ import (
 )
 
 func main() {
-	counts := make(map[string]int)
+	counts := make(map[string][]string)
 	files := os.Args[1:]
 	if len(files) == 0 {
 		countLines(os.Stdin, counts)
@@ -27,17 +27,21 @@ func main() {
 			f.Close()
 		}
 	}
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+	for line, filenames := range counts {
+		if len(filenames) > 1 {
+			fmt.Printf("%d\t%s\n", len(filenames), line)
+			for _, filename := range filenames {
+				fmt.Printf("\t%s\n", filename)
+			}
 		}
 	}
 }
 
-func countLines(f *os.File, counts map[string]int) {
+func countLines(f *os.File, counts map[string][]string) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
-		counts[input.Text()]++
+		line := input.Text()
+		counts[line] = append(counts[line], f.Name())
 	}
 	// NOTE: ignoring potential errors from input.Err()
 }
